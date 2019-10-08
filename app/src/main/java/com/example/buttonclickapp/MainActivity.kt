fea1516bc596d3f3a16ca55dfe04c010eee71cda
SNAp <tawatchaii2007@gmail.com>
2019-10-08 15:53:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.buttonclickapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var textView: TextView? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       val userInput = findViewById<EditText>(R.id.editText)
+       val button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+        userInput.setText("")
+
+        button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                textView?.append(userInput?.text)
+                textView?.append("\n")
+  //              userInput.text.clear()
+                userInput.setText("")
+
+            }
+        })
+    }
+}
